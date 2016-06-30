e6e0f03a212140e6c3870a68ad43785dd9fa7098
Cu7ious <garkusha.sergiy@gmail.com>
2016-06-30 08:26:12 +0000

    Fix & clear JSON output. Tested also /w curl & wget

--- app/routes/index.js
+++ app/routes/index.js
@@ -19,16 +19,23 @@ router.get('/', (req, response) => {
 })
 .get('/api/whoami', (req, response) => {
   const ip   = (req.headers['x-forwarded-for']) ? req.headers['x-forwarded-for'] : false
-  const lang = (req.headers['accept-language']) ? req.headers['accept-language'] : false
+  const lang = (req.headers['accept-language']) ? req.headers['accept-language'].substring(0,5) : false
   const ua   = (req.headers['user-agent']) ? req.headers['user-agent'].split(/\s*[;)(]\s*/) : false
 
-  console.log(ua)
-
-  response.json({
+  var result = {
     'ipaddress': ip,
-    'language' : lang,
-    'software' : (ua.length > 1) ? `${ua[1]}; ${ua[2]}` : ua[0]
-  })
+    'language' : lang
+  }
+
+  if (ua.length > 3) {
+    result.software = `${ua[1]}; ${ua[2]}`
+  } else if (ua.length > 1) {
+    result.software = `${ua[0]}; ${ua[1]}`
+  } else {
+    result.software = ua[0]
+  }
+
+  response.json(result)
 })
 
 module.exports = router
